756db02cff6472b7d9859c58979c79e2463f2397
na2na-p <yamamoto.akyshi@gmail.com>
2022-04-26 16:40:46 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+// import なず from './na2';
 import なず from './na2';
 
 debugger;
-new なず([]);
+new なず();
 debugger;
--- src/na2.ts
+++ src/na2.ts
@@ -1,24 +1,59 @@
-const pkg = require('../package.json');
-
-import Client from 'stream';
-import Module from 'module';
+import autobind from 'autobind-decorator';
+// eslint-disable-next-line object-curly-spacing
+import { EventEmitter } from 'events';
+import * as discord from 'discord.js';
 
 /**
- * なず
+ * Discord Client
  */
-export default class なず {
-  public readonly version = pkg.version;
-  private client: any;
-  public modules: Module[] = [];
+export default class なず extends EventEmitter {
+  public client :discord.Client;
 
   /**
-   * 本体
-   * @param {Module} modules モジュール
+   * コンストラクタ
+   * 接続を確立するやつ
    */
-  constructor(modules: Module[]) {
-    this.client = new Client();
-    console.log(this.client);
+  constructor() {
+    super();
+
+    this.client = new discord.Client({
+      intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES'],
+    });
     this.client.login(process.env.DISCORD_TOKEN);
-    this.modules = modules;
+    this.client.on('ready', this.onReady);
+    this.client.on('messageCreate', this.onMessage);
+  }
+
+  /**
+   * readyイベント
+   */
+  @autobind
+  private onReady() {
+    console.log(this.client.user?.tag);
+    this.emit('ready');
+  }
+
+  /**
+   * @param {discord.Message} receivedMessage 送りつけられてきたやつ
+   * @param {string} message 送信するメッセージ
+   */
+  public reply(receivedMessage:discord.Message, message: string): void {
+    receivedMessage.reply(message);
+    return;
+  }
+
+  /**
+   * messageCreateイベント
+   * 今後はここでそれぞれの機能モジュールのところへ行くようにする。
+   * コンストラクタでモジュールの読み込みをする。
+   * @param {discord.Message} message
+   */
+  private onMessage(message: discord.Message) {
+    if (message.author.bot) {
+      return;
+    }
+    console.log(message);
+    this.reply(message, 'おはようございます');
+    return;
   }
 }
