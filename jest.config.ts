3544f1baa6c01f8663200b130f82f63838657a4a
na2na-p <na2na@na2na.dev>
2023-01-07 01:00:23 +0900

    Update

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,9 @@
+import fs from 'fs';
+
 import type { JestConfigWithTsJest } from 'ts-jest';
+
+
+const config = JSON.parse(fs.readFileSync(`${__dirname}/.swcrc`, 'utf-8'));
 
 const jestConfig: JestConfigWithTsJest = {
 	extensionsToTreatAsEsm: ['.ts'],
@@ -6,7 +11,7 @@ const jestConfig: JestConfigWithTsJest = {
 		'^(\\.{1,2}/.*)\\.js$': '$1',
 	},
 	transform: {
-		'^.+\\.tsx?$': '@swc/jest',
+		'^.+\\.tsx?$': ['@swc/jest', { ...config }],
 	},
 };
 
