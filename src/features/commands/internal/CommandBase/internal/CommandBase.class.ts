5c48f2b64883186271c57360d354a3e36ed9eed6
na2na-p <na2na@na2na.dev>
2023-11-12 00:36:33 +0900

    説明追記

--- src/features/commands/internal/CommandBase/internal/CommandBase.class.ts
+++ src/features/commands/internal/CommandBase/internal/CommandBase.class.ts
@@ -1,7 +1,15 @@
 import type { InteractArgs } from './CommandBase.types.js';
 
 export abstract class CommandBase {
+  /**
+   * コマンド名
+   * この値がそのままコマンドの名前として表示&使用される
+   */
   public abstract readonly name: string;
+  /**
+   * コマンドの説明
+   * この値がそのままコマンドの説明として表示される
+   */
   public abstract readonly description: string;
 
   public register = () => this;
