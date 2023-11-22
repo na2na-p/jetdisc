27e947e2f46d7ae5184867e9e069c591148bfef0
na2na-p <na2na@na2na.dev>
2023-11-22 14:48:25 +0900

    テスト追加

--- src/features/core/internal/Voice/internal/Voice.class.ts
+++ src/features/core/internal/Voice/internal/Voice.class.ts
@@ -1,7 +1,6 @@
 import type {
   VoiceConnection,
   ChatInputCommandInteraction,
-  VoiceBasedChannel,
 } from '@/features/library/index.js';
 import {
   getVoiceConnection,
@@ -11,10 +10,13 @@ import {
 import { LogicException } from '@/features/others/Error/LogicException.js';
 import { getInteractionMemberId } from '@/features/others/discord/index.js';
 
-import { JOINABLE_TYPE_MAP } from './Voice.constants.js';
+import {
+  JOINABLE_TYPE_MAP,
+  checkJoinable,
+} from './funcs/checkJoinable/index.js';
 
 export class Voice {
-  #connection: Array<{
+  connection: Array<{
     guildId: string;
     connection: VoiceConnection;
   }> = [];
@@ -27,7 +29,7 @@ export class Voice {
     const {
       voice: { channel },
     } = await getInteractionMemberId(interaction);
-    const joinable = this.checkJoinable({
+    const joinable = checkJoinable({
       channel,
     });
 
@@ -64,15 +66,18 @@ export class Voice {
           adapterCreator: channel.guild.voiceAdapterCreator,
         });
 
-        this.#connection.push({
+        this.connection.push({
           guildId: channel.guild.id,
           connection,
         });
         return true;
 
       default:
-        throw new LogicException('Unknown joinable type.');
+        // HACK: ここでrejectするとSwitch外のカバレッジがuncoveredになる
+        break;
     }
+
+    return Promise.reject(new LogicException('Unknown joinable type.'));
   }
 
   public async leave({
@@ -83,7 +88,7 @@ export class Voice {
     const guildId = interaction.guildId;
     if (isNil(guildId)) return false;
 
-    const targetConnection = this.#connection.find(
+    const targetConnection = this.connection.find(
       connection => connection.guildId === guildId
     );
 
@@ -98,26 +103,10 @@ export class Voice {
       }
     } else {
       targetConnection.connection.destroy();
-      this.#connection = this.#connection.filter(
+      this.connection = this.connection.filter(
         connection => connection.guildId !== guildId
       );
       return true;
-    }
-  }
-
-  checkJoinable({
-    channel,
-  }: {
-    channel: VoiceBasedChannel | null;
-  }): (typeof JOINABLE_TYPE_MAP)[keyof typeof JOINABLE_TYPE_MAP] {
-    if (isNil(channel)) {
-      return JOINABLE_TYPE_MAP.NOT_FOUND;
-    } else if (!channel.joinable) {
-      return JOINABLE_TYPE_MAP.NOT_JOINABLE;
-    } else if (!channel.viewable) {
-      return JOINABLE_TYPE_MAP.NOT_VIEWABLE;
-    } else {
-      return JOINABLE_TYPE_MAP.JOINABLE;
     }
   }
 }
---
+++ src/features/core/internal/Voice/internal/funcs/checkJoinable/index.ts
@@ -0,0 +1,2 @@
+export { checkJoinable } from './internal/checkJoinable.func.js';
+export { JOINABLE_TYPE_MAP } from './internal/checkJoinable.constants.js';
