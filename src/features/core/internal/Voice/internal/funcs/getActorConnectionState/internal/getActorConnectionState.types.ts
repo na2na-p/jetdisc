5c62a34abba121ad7b985a302d6fd7cc1c30100b
na2na-p <na2na@na2na.dev>
2023-11-30 11:29:57 +0900

    interfaceではなくtypeで書く

--- src/features/core/internal/Voice/internal/funcs/getActorConnectionState/internal/getActorConnectionState.types.ts
+++ src/features/core/internal/Voice/internal/funcs/getActorConnectionState/internal/getActorConnectionState.types.ts
@@ -1,9 +1,7 @@
 import type { ConnectionState } from '@/features/core/index.js';
 import type { ChatInputCommandInteraction } from '@/features/library/index.js';
 
-export interface GetActorConnectionStateInterface {
-  (args: {
-    interaction: Readonly<ChatInputCommandInteraction>;
-    connections: Array<ConnectionState>;
-  }): Promise<ConnectionState | null>;
-}
+export type GetActorConnectionStateInterface = (args: {
+  interaction: Readonly<ChatInputCommandInteraction>;
+  connections: Array<ConnectionState>;
+}) => Promise<ConnectionState | null>;
