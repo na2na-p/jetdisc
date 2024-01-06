7c42b0c979fb0c616140fc32b9c0123b89652cdc
na2na-p <na2na@na2na.dev>
2024-01-06 12:11:12 +0900

    クレデンシャルの追加

--- src/features/config/internal/getConfig.func.ts
+++ src/features/config/internal/getConfig.func.ts
@@ -7,6 +7,8 @@ export const getConfig = () => {
       DISCORD_APP_TOKEN: z.string().min(1).readonly(),
       BOT_NAME: z.string().min(1).readonly(),
       SET_COMMANDS_TARGET_SERVERS: z.string().min(1).readonly(),
+      SPOTIFY_CLIENT_ID: z.string().min(1).readonly(),
+      SPOTIFY_CLIENT_SECRET: z.string().min(1).readonly(),
     } as const,
     runtimeEnv: process.env,
   });
